00a942dc9b7186baa391156804250cfbe29fea07
Josbert <whytejosbert@yahoo.com>
2024-09-11 04:31:17 +0000

    Added up

--- Basics.js
+++ Basics.js
@@ -66,3 +66,16 @@ document.querySelector("html").addEventListener("click", function(){
 document.querySelector("html").addEventListener("click", ()=>{
     alert("ouch! stop poking me");
 });
+ 
+//Adding an image changer
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png"){
+        myImage.setAttribute("src", "images/firefox2.png");
+    }else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
