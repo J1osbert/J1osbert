967e2e7c97797a35f2b92d2622243dddb08c831e
Josbert <whytejosbert@yahoo.com>
2024-09-11 07:55:56 +0000

    ADDED CHANGES

--- changes.js
+++ changes.js
@@ -1,2 +1,9 @@
 console.log("changes.js");
 console.log("version1");
+
+let myVariable="chocoloate";
+if (myVariable==="chocolate"){
+    alert("Yay, i love chocolate");
+}else{
+    alert("Awww, but i love ice cream");
+}
